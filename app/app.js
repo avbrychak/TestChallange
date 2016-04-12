96999640fd8d041ee17be38d9b085abc6dc42507
Anton Brychak <a.v.brychak@gmail.com>
2016-04-12 19:41:48 +0300

    added api request

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var http = require('http'),
     // NEVER use a Sync function except at start-up!
     index = fs.readFileSync(__dirname + '/index.html');
 var socketServer = require('socket.io');
+var request = require("request");
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
@@ -13,11 +14,17 @@ var app = http.createServer(function(req, res) {
 var io = socketServer.listen(app);
 
 var spawn = require('child_process').spawn,
-    ls = spawn('python',['main.py', "/dev/ttyUSB0"]);
+    ls = spawn('python',['main.py', "/dev/pts/22"]);
 
 ls.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
+    request({
+    	url: 'https://polar-falls-75205.herokuapp.com/api/bill_status',
+    	method: "POST",
+    	json: JSON.parse(data.toString())
+		}, function (error, response, body){
+        });
     io.emit('credit', { credit: data.toString() });
 });
 
-app.listen(3000);
+app.listen(3001);
